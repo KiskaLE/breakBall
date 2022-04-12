072564882305fa8cb749ca7b23aba3cb5f074e96
KiskaLE <vojtakylar@seznam.cz>
2022-04-12 20:30:04 +0200

    Game

--- BreakBall/src/PowerBlock.java
+++ BreakBall/src/PowerBlock.java
@@ -7,7 +7,6 @@ import java.awt.Graphics2D;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author vojtechkylar
@@ -17,23 +16,23 @@ public class PowerBlock extends Block {
     GamePanel panel;
 
     public PowerBlock(int x, int y, int height, int width, int hp, GamePanel panel) {
-        super(x, y, height, width, hp);
+        super(x, y, height, width, hp, panel);
         this.panel = panel;
     }
 
-    public void setPower() {
+    public void set() {
+        panel.score += 100;
 
-        if (Math.random() > 0.5) {
-            panel.ball.xSpeed += 1;
-            panel.ball.ySpeed += 1;
-        }else{
-            panel.ball.xSpeed = 4;
-            panel.ball.ySpeed = 4;
+        for (Ball ball : panel.balls) {
+            ball.xSpeed = panel.defaultSpeed + 2;
+            ball.ySpeed += panel.defaultSpeed + 2;
         }
-        
+        panel.timer = 200l;
+
     }
+
     @Override
-    public void draw(Graphics2D gtd){
+    public void draw(Graphics2D gtd) {
         gtd.setColor(new Color(252, 177, 217));
         gtd.fillRect(x, y, width, height);
     }
