fc0a9d59b2961eb4956d5853ee82a82fe9db121f
KiskaLE <vojtakylar@seznam.cz>
2022-04-14 15:11:26 +0200

    Game

---
+++ BreakBall/src/HiddenBlock.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author vojtechkylar
+ */
+public class HiddenBlock extends Block {
+
+    public HiddenBlock(GamePanel panel) {
+        super.panel = panel;
+    }
+
+    @Override
+    public void set() {
+
+    }
+
+    @Override
+    public void draw(Graphics2D gtd) {
+
+    }
+
+}
